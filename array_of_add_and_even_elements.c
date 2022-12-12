7a90f85c386e517321425adb5054aed0535b7649
satish22a91a0591 <119840066+satish22a91a0591@users.noreply.github.com>
2022-12-12 11:50:51 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);         if(a[i]%2!=0)         {             printf("%d ",a[i]);         }     }     for(int i=0;i<n;i++)     {         if(a[i]%2==0)         {             printf("%d ",a[i]);         }     } }

---
+++ array_of_add_and_even_elements.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]%2!=0)
+        {
+            printf("%d ",a[i]);
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            printf("%d ",a[i]);
+        }
+    }
+}
