ed452437eedfd2ee51afda168f03ae60dee52035
Satish Yadav <119840066+satish22a91a0591@users.noreply.github.com>
2023-05-09 00:11:22 +0530

    #include<stdio.h> int prime(int a) {     int c=0;     for(int i=1;i<=a;i++)     {         if(a%i==0)         {             c++;         }     }     if(c==2)     {         return 1;     }     else     {         return 0;     } } int main() {         int x,f;         scanf("%d%d",&x,&f);         for(int i=x;i<=f;i++)         {             if(prime(i))             {                 printf("%d ",i);             }         }     //printf("%d",cnt); }

--- Generate_Prime_Numbers_In_A_Interval_.c
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -1,22 +1,34 @@
 #include<stdio.h>
-int main()
+int prime(int a)
 {
-    int a,b;
-    scanf("%d%d",&a,&b);
-    for(int i=a;i<=b;i++)
+    int c=0;
+    for(int i=1;i<=a;i++)
     {
-        int c=0;
-        for(int j=1;j<=i;j++)
+        if(a%i==0)
         {
-            if(i%j==0)
-            {
-                c++;
-            }
-        }
-        if(c==2)
-        {
-            printf("%d
-",i);
+            c++;
         }
     }
+    if(c==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+        int x,f;
+        scanf("%d%d",&x,&f);
+        for(int i=x;i<=f;i++)
+        {
+            if(prime(i))
+            {
+                printf("%d
+",i);
+            }
+        }
+    //printf("%d",cnt);    
 }
