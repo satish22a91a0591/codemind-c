c9a87fab7fa7e1c0f012f5253e607b75ec0bec13
Satish Yadav <119840066+satish22a91a0591@users.noreply.github.com>
2023-05-17 23:37:21 +0530

    #include<stdio.h> int main() {     int n,s=0;     scanf("%d",&n);     int a[n];     for(int i=1;i<=n;i++)     {         scanf("%d",&a[i]);         if(a[i]%2!=0)         {             break;         }         s=s+a[i];     }     printf("%d",s); }

---
+++ sum_of_array__elements_upto__first_odd_number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,s=0;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]%2!=0)
+        {
+            break;
+        }
+        s=s+a[i];
+    }
+    printf("%d",s);
+}
