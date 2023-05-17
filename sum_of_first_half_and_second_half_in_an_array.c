2ca19282c1fd4647996874b8e5509dec3c91b66a
Satish Yadav <119840066+satish22a91a0591@users.noreply.github.com>
2023-05-17 23:26:08 +0530

    #include<stdio.h> int main() {     int n,sm=0,s=0;     scanf("%d",&n);     int a[n];     for(int i=1;i<=n;i++)     {         scanf("%d",&a[i]);     }     for(int i=1;i<=n/2;i++)     {         s=s+a[i];     }     for(int i=(n/2)+1;i<=n;i++)     {         sm=sm+a[i];     }     printf("%d %d",s,sm); }

---
+++ sum_of_first_half_and_second_half_in_an_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,sm=0,s=0;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=1;i<=n/2;i++)
+    {
+        s=s+a[i];
+    }
+    for(int i=(n/2)+1;i<=n;i++)
+    {
+        sm=sm+a[i];
+    }
+    printf("%d
+%d",s,sm);
+}
