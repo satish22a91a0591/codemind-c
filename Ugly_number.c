e892aaceb63d226e366e0ff625bb35cbee0786e1
Satish Yadav <119840066+satish22a91a0591@users.noreply.github.com>
2023-05-22 15:13:44 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     while(n!=1)     {         if(n%5==0)         {             n=n/5;         }         else if(n%3==0)         {             n=n/3;         }         else if(n%2==0)         {             n=n/2;         }         else         {             printf("Not Ugly Number");             return 0;         }     }     printf("Ugly Number"); }

--- Ugly_number.c
+++ Ugly_number.c
@@ -3,31 +3,25 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    while(n!=0)
+    while(n!=1)
     {
-        if(n%2==0)
+        if(n%5==0)
         {
-            n=n/2;
+            n=n/5;
         }
         else if(n%3==0)
         {
             n=n/3;
         }
-        else if(n%5==0)
+        else if(n%2==0)
         {
-            n=n/5;
+            n=n/2;
         }
         else
         {
-            break;
+            printf("Not Ugly Number");
+            return 0;
         }
     }
-    if(n==1)
-    {
-        printf("Ugly Number");
-    }
-    else
-    {
-        printf("Not Ugly Number");
-    }
+    printf("Ugly Number");
 }
