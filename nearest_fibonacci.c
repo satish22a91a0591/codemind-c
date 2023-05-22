8965417f8e83855130fa3a0096fe55727d76f13e
Satish Yadav <119840066+satish22a91a0591@users.noreply.github.com>
2023-05-22 08:02:17 +0530

    #include<stdio.h> int fib(int n) {     int a,b;     a=0;     b=1;     int c=a+b;     while(n>c)     {         a=b;         b=c;         c=a+b;     }     if(c==n)     {         return 1;     }     else     {         return 0;     } } int main() {     int n,back,forw;     scanf("%d",&n);     for(int i=n;;i++)     {         if(fib(i))         {             forw=i;             break;         }     }     for(int i=n-1;;i--)     {         if(fib(i))         {             back=i;             break;         }     }     int d1=forw-n;     int d2=n-back;     if(d1==d2)     {         printf("%d %d",back,forw);     }     else if(d1>d2)     {         printf("%d",back);     }     else     {         printf("%d",forw);     } }

--- nearest_fibonacci.c
+++ nearest_fibonacci.c
@@ -1,44 +1,57 @@
 #include<stdio.h>
-int main()
+int fib(int n)
 {
-    int n,a=0,b=1,x=0,y=1,c,z,w,m;
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++)
+    int a,b;
+    a=0;
+    b=1;
+    int c=a+b;
+    while(n>c)
     {
-         if(a>n)
-        {
-            w=c-a;
-            break;
-        }
-        c=a+b;
         a=b;
         b=c;
+        c=a+b;
     }
-    
-    for(int j=1;j<=n;j++)
+    if(c==n)
     {
-        if(x>n)
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int n,back,forw;
+    scanf("%d",&n);
+    for(int i=n;;i++)
+    {
+        if(fib(i))
         {
-            m=x;
+            forw=i;
             break;
         }
-        z=x+y;
-        x=y;
-        y=z;
-    } 
-    int d1=n-w;
-    int d2=m-n;
-    if(d1<d2)
-    {
-        printf("%d",w);
     }
-    else if(d2<d1)
+    for(int i=n-1;;i--)
     {
-        printf("%d",m);
+        if(fib(i))
+        {
+            back=i;
+            break;
+        }
     }
-    else if(d1==d2)
+    int d1=forw-n;
+    int d2=n-back;
+    if(d1==d2)
     {
-        printf("%d %d",w,m);
+        printf("%d %d",back,forw);
     }
-    
+    else if(d1>d2)
+    {
+        printf("%d",back);
+    }
+    else
+    {
+        printf("%d",forw);
+    }
 }
