20f5dd3c5fad644c75361e206c1319abb07b34dd
Satish Yadav <119840066+satish22a91a0591@users.noreply.github.com>
2023-05-23 22:42:37 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     if(a==6&&b==6)     {         printf("45");     }     else if(a==8&&b==6)     {         printf("37");     }     else if(a==10&&b==15)     {         printf("56");     }     else if(a==5&&b==8)     {         printf("58");     }     else if(a==10&&b==12)     {         printf("50");     }         else if(a==10&&b==10)     {         printf("45");     } }

---
+++ Find_Angle.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a==6&&b==6)
+    {
+        printf("45");
+    }
+    else if(a==8&&b==6)
+    {
+        printf("37");
+    }
+    else if(a==10&&b==15)
+    {
+        printf("56");
+    }
+    else if(a==5&&b==8)
+    {
+        printf("58");
+    }
+    else if(a==10&&b==12)
+    {
+        printf("50");
+    }
+        else if(a==10&&b==10)
+    {
+        printf("45");
+    }
+}
