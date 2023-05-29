ab7845a006f65b7411eeff4ae2d7b65e6491ed8a
Satish Yadav <119840066+satish22a91a0591@users.noreply.github.com>
2023-05-29 21:46:20 +0530

    #include<stdio.h> int main() {     int n,x=0;     scanf("%d",&n);     int cnt=1;     for(int i=2;i<n;i++)     {         if(n%i==0)         {             printf("%d ",i);             cnt=0;         }     }     if(cnt==1)     {         printf("-1");     } }

--- Find_two_distinct_prime_numbers_with_given_product.c
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n,c=0;
+    int n,x=0;
     scanf("%d",&n);
+    int cnt=1;
     for(int i=2;i<n;i++)
     {
         if(n%i==0)
         {
             printf("%d ",i);
-            c++;
+            cnt=0;
         }
     }
-    if(c==0)
+    if(cnt==1)
     {
         printf("-1");
     }
