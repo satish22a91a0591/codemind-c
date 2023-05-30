f14e11501d61a85dd399b47dca3eac15b0669697
Satish Yadav <119840066+satish22a91a0591@users.noreply.github.com>
2023-05-30 14:58:20 +0530

    #include<stdio.h> int main() {     int n,c=0,cnt=0;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);         if(i%2==0)         {             if(a[i]%2==0)             {                 c++;             }         }         else         {             cnt++;         }     }     if(cnt+c==n)     {         printf("True");     }     else     {         printf("False");     } }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -1,31 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int n,c=0,count=0;
+    int n,c=0,cnt=0;
     scanf("%d",&n);
     int a[n];
     for(int i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
-    }
-    for(int i=0;i<n;i++)
-    {
         if(i%2==0)
         {
-            if(a[i]%2!=0)
+            if(a[i]%2==0)
             {
                 c++;
             }
         }
         else
         {
-            if(a[i]%2==0)
-            {
-                count++;
-            }
+            cnt++;
         }
     }
-    if(c==0)
+    if(cnt+c==n)
     {
         printf("True");
     }
