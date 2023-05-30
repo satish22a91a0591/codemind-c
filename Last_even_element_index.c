ddbe9931d482d5cf988f9e048617687a50030b17
Satish Yadav <119840066+satish22a91a0591@users.noreply.github.com>
2023-05-30 13:51:36 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(int i=n-1;i>=0;i--)     {         if(a[i]%2==0)         {             printf("%d",i);             return 0;         }     } }

---
+++ Last_even_element_index.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=n-1;i>=0;i--)
+    {
+        if(a[i]%2==0)
+        {
+            printf("%d",i);
+            return 0;
+        }
+    }
+}
