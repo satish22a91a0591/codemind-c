cb2f48a770fcece1ab40af2c719a08b02ca43f54
Satish Yadav <119840066+satish22a91a0591@users.noreply.github.com>
2023-06-01 14:17:47 +0530

    #include<stdio.h> int prime(int n) {     int c=0;     for(int i=1;i<=n;i++)     {         if(n%i==0)         {             c++;         }     }     if(c==2)     {         return 1;     }     else     {         return 0;     } } int main(){     int n,r,c=0,cnt=0;     scanf("%d",&n);     if(prime(n))     {         while(n!=0)         {             r=n%10;             c++;             if(prime(r))             {                 cnt++;             }             n=n/10;         }         if(c==cnt)         {             printf("Mega Prime");         }         else{             printf("Not Mega Prime");         }     }     else     {         printf("Not Mega Prime");     } }

--- mega_prime.c
+++ mega_prime.c
@@ -1,53 +1,48 @@
-# include<stdio.h>
-int prime(int );
-int main()
-{
-int n,c=0,count=0,r;
-scanf("%d",&n);
-if(prime(n))
-{
-	while(n!=0)
-	{
-r=n%10;
-c++;
-if(prime(r))
-{
-	count++;
-	}
-	n=n/10;	
-}
-
-if(c==count)
-{
-	printf("Mega Prime");
-}
-else
-{
-	printf("Not Mega Prime");
-}
-}
- else 
-{
-	printf("Not Mega Prime");
-}
-	
-}
+#include<stdio.h>
 int prime(int n)
 {
-	int c=0;
-	for(int i=1; i<=n; i++)
-	{
-		if(n%i==0)
-		{
-			c++;
-		}
-	}
-	if(c==2)
-	{
-		return 1;
-	}
-	else 
-	{
-		return 0;
-	}
+    int c=0;
+    for(int i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main(){
+    int n,r,c=0,cnt=0;
+    scanf("%d",&n);
+    if(prime(n))
+    {
+        while(n!=0)
+        {
+            r=n%10;
+            c++;
+            if(prime(r))
+            {
+                cnt++;
+            }
+            n=n/10;
+        }
+        if(c==cnt)
+        {
+            printf("Mega Prime");
+        }
+        else{
+            printf("Not Mega Prime");
+        }
+    }
+    else
+    {
+        printf("Not Mega Prime");
+    }
 }
