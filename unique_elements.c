2b281bd23a4cb793e9cb404a859998789b265f0a
Satish Yadav <119840066+satish22a91a0591@users.noreply.github.com>
2023-06-03 11:06:03 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(int i=0;i<n;i++)     {         for(int j=i+1;j<n;j++)         {             if(a[i]==a[j])             {                 for(int k=j;k<n-1;k++)                 {                     a[k]=a[k+1];                 }                 j--;                 n--;             }         }     }     for(int i=0;i<n;i++)     {         printf("%d ",a[i]);     }
    }

--- unique_elements.c
+++ unique_elements.c
@@ -12,14 +12,14 @@ int main()
     {
         for(int j=i+1;j<n;j++)
         {
-            if(a[j]==a[i])
+            if(a[i]==a[j])
             {
                 for(int k=j;k<n-1;k++)
                 {
                     a[k]=a[k+1];
                 }
-                n--;
                 j--;
+                n--;
             }
         }
     }
@@ -27,4 +27,5 @@ int main()
     {
         printf("%d ",a[i]);
     }
+    
 }
